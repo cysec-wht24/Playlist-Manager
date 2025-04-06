cbc9eac2ccceb48bcb1d7c20d91e13eaa36cab2d
Man-Of-May <bmanomay2018@gmail.com>
2025-04-06 06:35:24 +0530

    Designing

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+// https://nextjs.org/docs/app/building-your-application/rendering/client-components
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+import {axios} from "axios";
+// ts requires type defination since axios 
+// type are not defined you have to explicitly install them
+
+
 export default function Page() {
-    return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-  }
+
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: ""
+  })
+  
+  const onSignUp = async () => {
+
+  } 
+
+    return (
+    <h1 className="text-3xl font-bold 
+    underline flex flex-col items-center justify-center min-h-screen py-2"> SignUp </h1>
+  )}

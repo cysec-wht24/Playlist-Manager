584c32f477e8e1f421adafb45818e32ce7604dc9
Man-Of-May <bmanomay2018@gmail.com>
2025-04-11 06:17:59 +0530

    profile cover full screen

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -105,7 +105,7 @@ export default function SidebarDemo() {
   return (
     <div
       className={cn(
-        "mx-auto flex w-full max-w-7xl flex-1 flex-col overflow-hidden rounded-md border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
+        "mx-auto flex w-full flex-1 flex-col overflow-hidden rounded-md border border-neutral-200 bg-gray-100 md:flex-row dark:border-neutral-700 dark:bg-neutral-800",
         "h-screen", // for your use case, use `h-screen` instead of `h-[60vh]`
       )}
     >

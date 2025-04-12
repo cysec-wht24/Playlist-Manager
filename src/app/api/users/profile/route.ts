6bad5d5434290ef3dfcea5d9ee7a38570dcc16d7
Man-Of-May <bmanomay2018@gmail.com>
2025-04-13 02:53:17 +0530

    setting structure of route

---
+++ src/app/api/users/profile/route.ts
@@ -0,0 +1,15 @@
+import {connect} from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import Playlist from "@/models/playlistModel";
+
+connect()
+
+export async function POST(request: NextRequest) {}
+
+export async function GET(request: NextRequest) {}
+
+export async function PATCH(request: NextRequest) {}
+
+export async function DELETE(request: NextRequest) {}
+
+ 
